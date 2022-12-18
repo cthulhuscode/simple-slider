4ae6faa1d09d51dc90713400fd232fb85df154f8
Enrique Ruvalcaba <jordison352@gmail.com>
2022-12-18 08:39:01 -0600

    images 100vw

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const images = ["1.jpeg", "2.webp", "3.webp"];
 const slider = document.querySelector(".slider");
 
 slider.innerHTML += images
-  .map((img) => {
-    return `
-      <img src="./images/${img}">
-    `;
-  })
+  .map((img) => `<img src="./images/${img}" style="width: 100vw;">`)
   .join("");
 
 const leftArrow = document.querySelector(".arrow-left");
@@ -14,13 +10,17 @@ const rightArrow = document.querySelector(".arrow-right");
 let slideCounter = 0;
 
 rightArrow.addEventListener("click", () => {
+  const width = window.innerWidth;
+
   slideCounter++;
   if (slideCounter === 3) slideCounter = 0;
-  slider.style.transform = `translateX(${slideCounter * -1600}px)`;
+  slider.style.transform = `translateX(${slideCounter * -width}px)`;
 });
 
 leftArrow.addEventListener("click", () => {
+  const width = window.innerWidth;
+
   slideCounter--;
   if (slideCounter < 0) slideCounter = 2;
-  slider.style.transform = `translateX(${slideCounter * -1600}px)`;
+  slider.style.transform = `translateX(${slideCounter * -width}px)`;
 });
